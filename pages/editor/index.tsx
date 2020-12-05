22deff2b5f0dcd7e348bafdae928f47829a9876d
m19e <nexus.me7290@gmail.com>
2020-12-06 07:37:14 +0900

    Styling, set footer

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import VerticalEditor from "../../components/Vertical";
+import Footer from "../../components/Footer";
+
+const styles: { [key: string]: CSSProperties } = {
+    wrapper: {
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+    },
+    flexItem: {
+        flex: 1,
+    },
+};
 
 export default function EditorIndex() {
-    return <VerticalEditor />;
+    return (
+        <div style={styles.wrapper}>
+            <div style={styles.flexItem}>
+                <VerticalEditor />
+                <Footer />
+            </div>
+        </div>
+    );
 }
