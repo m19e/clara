7b78476b7d6554189173b22d2d44f57ced11b738
m19e <nexus.me7290@gmail.com>
2020-12-06 07:36:06 +0900

    Add footer sample

---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+import React, { CSSProperties } from "react";
+
+const styles: { [key: string]: CSSProperties } = {
+    footer: {
+        backgroundColor: "lightgray",
+        minHeight: "120px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    control: {
+        backgroundColor: "white",
+        width: "90px",
+        height: "90px",
+        margin: "8px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+};
+
+export default function Footer() {
+    return (
+        <div style={styles.footer}>
+            {["", "", ""].map((_) => (
+                <div style={styles.control}>
+                    <p>control</p>
+                </div>
+            ))}
+        </div>
+    );
+}
