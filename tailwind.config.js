1685db14ec20f9b0bf619cea70f9a4332af60a69
m19e <nexus.me7290@gmail.com>
2020-12-08 12:32:29 +0900

    Use extend instead of override

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,10 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-        minHeight: {
-            32: "8rem",
+        extend: {
+            minHeight: {
+                32: "8rem",
+            },
         },
     },
     variants: {
