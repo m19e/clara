d71d1498f6affd1139a34d983657ea39a93f0b0c
m19e <nexus.me7290@gmail.com>
2020-12-11 01:22:38 +0900

    Style novels list view

--- pages/fs/index.tsx
+++ pages/fs/index.tsx
@@ -15,17 +15,49 @@ const WithStaticProps = ({ novels }: Props) => {
                 {novels.map((novel, i) => (
                     <div key={i} className="flex">
                         {/* <p>{JSON.stringify(novel)}</p> */}
-                        <div className="w-80 m-4">
-                            <p>{novel.id}</p>
-                            <p>{novel.author}</p>
-                            <p>{novel.title}</p>
-                            <p>{novel.description}</p>
-                            <p>{novel.text}</p>
-                            <p>{novel.charCount}</p>
-                            <p>{novel.tags}</p>
-                            <p>{novel.bookmarkCount}</p>
-                            <p>{novel.visitCount}</p>
-                            <p>{novel.created_at}</p>
+                        <div className="flex items-start m-4 p-4 border-b-2" style={{ width: "392px" }}>
+                            <img src="http://via.placeholder.com/96x120" alt="表紙画像のplaceholder" />
+                            <div className="mx-6">
+                                {/* <p>{novel.id}</p> */}
+                                {/* <p>{novel.author}</p> */}
+                                <p>{novel.title}</p>
+                                <div className="text-gray-400 flex items-center" style={{ fontSize: "12px" }}>
+                                    {/* <p>{novel.charCount}</p> */}
+                                    <p className="my-1">3500文字</p>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="icon icon-tabler icon-tabler-heart ml-1"
+                                        width="12"
+                                        height="12"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="#9e9e9e"
+                                        fill="currentcolor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                                    </svg>
+                                    {/* <p>{novel.bookmarkCount}</p> */}
+                                    <p>12</p>
+                                </div>
+                                {/* <p>{novel.tags}</p> */}
+                                <div className="flex flex-wrap">
+                                    {["アイドルマスターシャイニーカラーズ", "樋口円香", "福丸小糸", "まどこい"].map((tag, i) => (
+                                        <p key={"00" + i} className="text-blue-400 mr-1" style={{ fontSize: "14px" }}>
+                                            #{tag}
+                                        </p>
+                                    ))}
+                                </div>
+                                {/* <p>{novel.description}</p> */}
+                                <p style={{ fontSize: "14px", width: "240px", height: "60px", textOverflow: "ellipsis" }}>
+                                    これはキャプションですこれはキャプションですこれはキャプションですこれはキャプションですこれはキャプションですこれはキャプションです
+                                </p>
+                                {/* <p>{novel.text}</p> */}
+                                {/* <p>{novel.visitCount}</p> */}
+                                {/* <p>{novel.created_at}</p> */}
+                            </div>
                         </div>
                     </div>
                 ))}
