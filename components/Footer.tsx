f9a5921f4822fb5a108465f7bc23c9534fbf6ca8
m19e <nexus.me7290@gmail.com>
2020-12-21 21:29:42 +0900

    Set divider display and control

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,10 +12,15 @@ export default function Footer() {
                 <div className="w-24 h-24 m-2 flex-center">
                     <div className="group flex flex-col">
                         <span className="h-6"></span>
-                        <span className="w-full text-center">{isMincho ? "明朝体" : "ゴシック体"}</span>
+                        <span className="w-full text-center border-t border-b border-solid border-black border-opacity-20">
+                            {isMincho ? "明朝体" : "ゴシック体"}
+                        </span>
                         <button
                             onClick={() => toggleFont()}
-                            className={"transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-100" + (!isMincho ? " mincho" : " gothic")}
+                            className={
+                                "outline-none focus:outline-none transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-100" +
+                                (!isMincho ? " mincho" : " gothic")
+                            }
                         >
                             {!isMincho ? "明朝体" : "ゴシック体"}
                         </button>
@@ -41,7 +46,7 @@ export default function Footer() {
                                 </svg>
                             </span>
                         </button>
-                        <p>大きさ {fontSize}</p>
+                        <p className="border-t border-b border-solid border-black border-opacity-20">大きさ{fontSize}</p>
                         <button
                             className="outline-none focus:outline-none transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-100"
                             onClick={() => decFontSize()}
@@ -82,7 +87,7 @@ export default function Footer() {
                                 </svg>
                             </span>
                         </button>
-                        <p>字数 {lineWords}</p>
+                        <p className="border-t border-b border-solid border-black border-opacity-20">字数 {lineWords}</p>
                         <button
                             className="outline-none focus:outline-none transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-100"
                             onClick={() => decLineWords()}
