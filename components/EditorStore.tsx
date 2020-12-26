50e7a45aaee2e7281e28bbd97a0d91cde560d1f2
m19e <nexus.me7290@gmail.com>
2020-12-26 10:54:25 +0900

    Add create draft button

--- components/EditorStore.tsx
+++ components/EditorStore.tsx
@@ -127,10 +127,21 @@ const VerticalEditor = () => {
                 <style>{`* { margin: 0px; overflow: hidden; }`}</style>
             </Head>
             <div className={"fixed top-0 w-full" + (isMincho ? " mincho" : " gothic")}>
-                <div className="flex items-center justify-between">
-                    <span></span>
-                    <p className="p-5 text-black opacity-50">{title}</p>
-                    <span></span>
+                <div className="flex justify-between items-center">
+                    <div className="w-20"></div>
+                    <p className="text-black opacity-50 py-5 flex-grow-0">{title}</p>
+                    <div className="w-20">
+                        <div className="flex justify-end items-center">
+                            <span className="px-4 opacity-50">
+                                <svg className="w-8 h-8" width="21" height="21" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg">
+                                    <g fill="none" fill-rule="evenodd" stroke="#2a2e3b" strokeLinecap="round" strokeLinejoin="round">
+                                        <path d="m5.029 10.429h10" />
+                                        <path d="m10.029 15.429v-10.001" />
+                                    </g>
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="min-h-screen flex flex-col editor-bg">
