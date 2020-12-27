ad8b354951f1b49f0d8f9d96a62bfb0e4efb769e
m19e <nexus.me7290@gmail.com>
2020-12-27 12:47:03 +0900

    Fix tag prop name

--- components/EditorStore.tsx
+++ components/EditorStore.tsx
@@ -134,7 +134,7 @@ const VerticalEditor = () => {
                         <div className="flex justify-end items-center">
                             <span className="px-4 opacity-50">
                                 <svg className="w-8 h-8" width="21" height="21" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg">
-                                    <g fill="none" fill-rule="evenodd" stroke="#2a2e3b" strokeLinecap="round" strokeLinejoin="round">
+                                    <g fill="none" fillRule="evenodd" stroke="#2a2e3b" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="m5.029 10.429h10" />
                                         <path d="m10.029 15.429v-10.001" />
                                     </g>
