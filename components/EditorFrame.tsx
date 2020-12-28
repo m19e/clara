3dbb32e7cf38aec5e020e642d098d128098e803f
m19e <nexus.me7290@gmail.com>
2020-12-29 00:40:48 +0900

    Handle ismincho class on frame

--- components/EditorFrame.tsx
+++ components/EditorFrame.tsx
@@ -6,7 +6,7 @@ export default function Frame() {
     const [isMincho] = useIsMincho();
 
     return (
-        <div className="transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100">
+        <div className={"transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100" + (isMincho ? " mincho" : " gothic")}>
             <Header />
             <Footer />
         </div>
