15490e22a82e4fbb9e856c9dcbf1d51e3722a9b0
m19e <nexus.me7290@gmail.com>
2020-12-31 20:40:48 +0900

    Edit title

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -1,30 +1,66 @@
+import React, { useState } from "react";
 import { useTitle } from "../store/draft";
 
 export default function Header() {
     const [title, setTitle] = useTitle();
+    const [isTitleEdit, setIsTitleEdit] = useState(false);
+
+    const toggleTitleEdit = () => {
+        setIsTitleEdit((prev) => !prev);
+    };
+
+    const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+
     return (
         <div className="shadow-sm editor-bg fixed top-0 w-full">
             <div className="flex justify-between items-center">
                 <div className="w-20"></div>
                 <div className="py-5 flex-grow-0 flex-center group">
                     <div className="w-6"></div>
-                    <p className="text-black opacity-75 px-1">{title}</p>
-                    <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-50">
-                        <svg
-                            className="w-4 h-4 opacity-50 hover:opacity-100"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="#2a2e3b"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                    {isTitleEdit ? (
+                        <>
+                            <input
+                                className="text-black opacity-75 px-2"
+                                type="text"
+                                value={title}
+                                onChange={(e) => onTitleChange(e)}
+                                style={{ width: title.length + 2 + "rem" }}
                             />
-                        </svg>
-                    </div>
+                            <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-50" onClick={toggleTitleEdit}>
+                                <svg
+                                    className="w-4 h-4 opacity-50 hover:opacity-100"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="#2a2e3b"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                                </svg>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <p className="text-black opacity-75 px-1">{title}</p>
+                            <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-50" onClick={toggleTitleEdit}>
+                                <svg
+                                    className="w-4 h-4 opacity-50 hover:opacity-100"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="#2a2e3b"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                                    />
+                                </svg>
+                            </div>
+                        </>
+                    )}
                 </div>
                 <div className="w-20">
                     <div className="flex justify-end items-center">
