441df9fd2059c44961917586629ec64ab30ec904
m19e <nexus.me7290@gmail.com>
2020-12-31 16:45:04 +0900

    Add title edit icon

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -6,7 +6,20 @@ export default function Header() {
         <div className="shadow-sm editor-bg fixed top-0 w-full">
             <div className="flex justify-between items-center">
                 <div className="w-20"></div>
-                <p className="text-black opacity-75 py-5 flex-grow-0">{title}</p>
+                <div className="py-5 flex-grow-0 flex-center">
+                    <div className="w-6"></div>
+                    <p className="text-black opacity-75 px-1">{title}</p>
+                    <div className="w-6">
+                        <svg className="opacity-50 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#2a2e3b">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                            />
+                        </svg>
+                    </div>
+                </div>
                 <div className="w-20">
                     <div className="flex justify-end items-center">
                         <span className="px-4 opacity-50">
