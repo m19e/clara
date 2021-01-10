0a62d166de0184ea6c0c2e8695333d3463bf257a
m19e <nexus.me7290@gmail.com>
2021-01-11 04:04:31 +0900

    Remove auth provider from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import { AppProps } from "next/app";
-import { AuthProvider } from "../auth/AuthProvider";
 import "../styles/global.css";
 import "../styles/loader.css";
 import "../styles/font.css";
 
-const App = ({ Component, pageProps }: AppProps) => (
-    <AuthProvider>
-        <Component {...pageProps} />
-    </AuthProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default App;
