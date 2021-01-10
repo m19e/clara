6ce08618cc6085cf7207683503f21c6dfa39531e
m19e <nexus.me7290@gmail.com>
2021-01-11 06:34:56 +0900

    Set temp login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,24 +1,18 @@
 import { GetServerSideProps } from "next";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, loginWithTwitter, logout } from "../../lib/firebase/initFirebase";
+import { loginWithTwitter } from "../../lib/firebase/initFirebase";
 
 type Props = {};
 
 export default function LoginIndex() {
-    const [user, loading] = useAuthState(auth);
-    console.log(user);
-
     return (
-        <div className="flex justify-center items-center flex-1">
-            <div className="w-48 h-48 flex justify-center items-center">
-                {!loading && user ? (
-                    <div>
-                        <button onClick={() => logout()}>Logout</button>
-                        <h2>Status: You Logged In</h2>
-                    </div>
-                ) : (
-                    <button onClick={() => loginWithTwitter()}>Login</button>
-                )}
+        <div className="flex-center flex-1 h-screen bg-gray-100">
+            <div className="w-80 h-60 flex-center flex-col editor-bg rounded">
+                <h1 className="text-7xl">Clara</h1>
+                <div className="btn-twitter flex-center w-72 mt-4 rounded-2xl cursor-pointer" onClick={() => loginWithTwitter()}>
+                    <span className="system-font opacity-60 py-1" style={{ fontSize: "14px" }}>
+                        Twitterでログイン
+                    </span>
+                </div>
             </div>
         </div>
     );
