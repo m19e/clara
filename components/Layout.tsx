5a589dcf6d998a37361c6745fb2fa2c2427045c2
m19e <nexus.me7290@gmail.com>
2021-01-12 05:34:24 +0900

    Replace component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,28 +1,12 @@
+import JumpEditorButton from "./JumpEditorButton";
+
 export default function Layout() {
     return (
         <div className="min-h-screen w-full bg-gray-200">
             <nav className="h-16 w-full flex-center editor-bg">
                 <div className="container flex flex-wrap items-center justify-between">
                     <h1 className="mincho font-black text-3xl text-gray-600 mx-4">Clara</h1>
-                    <div className="flex-center mx-4 px-6 bg-gray-200 bg-opacity-60 rounded-2xl cursor-pointer">
-                        <span className="gothic font-black opacity-60 py-2" style={{ fontSize: "14px" }}>
-                            小説を書く
-                        </span>
-                        <svg
-                            className="w-4 h-4 opacity-50 hover:opacity-100"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                            />
-                        </svg>
-                    </div>
+                    <JumpEditorButton />
                 </div>
             </nav>
         </div>
