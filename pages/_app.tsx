662595c033e5049853633f314a6185aa8b45303d
m19e <nexus.me7290@gmail.com>
2021-01-12 07:10:50 +0900

    Wrap _app by recoilroot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import "../styles/global.css";
 import "../styles/loader.css";
 import "../styles/font.css";
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+    <RecoilRoot>
+        <Component {...pageProps} />
+    </RecoilRoot>
+);
 
 export default App;
