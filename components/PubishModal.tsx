4768e338e52b1f9e2e9db13f449b4213e175358d
m19e <nexus.me7290@gmail.com>
2021-01-15 06:51:47 +0900

    Add publish modal

---
+++ components/PubishModal.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function PublishModal() {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+            <svg
+                className="w-6 h-6 opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => setShowModal(true)}
+            >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+            </svg>
+            {showModal && (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        <div className="relative w-auto my-6 mx-auto max-w-sm">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full p-6 bg-white outline-none focus:outline-none">
+                                <span>test</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            )}
+        </>
+    );
+}
