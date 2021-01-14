ffd2566c45f454bc68c9e78fc3e6c9ea256621df
m19e <nexus.me7290@gmail.com>
2021-01-15 06:27:36 +0900

    Enlarge icon button

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -51,7 +51,7 @@ export default function Header() {
                         <span className="px-4 opacity-50">
                             <Link href="/">
                                 <a>
-                                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                                     </svg>
                                 </a>
@@ -99,7 +99,7 @@ export default function Header() {
                     <div className="flex justify-end items-center">
                         <span className="px-4 opacity-50">
                             <svg
-                                className="w-5 h-5"
+                                className="w-6 h-6"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 width="24"
