5f971cf5244f0da43a94e12498a938c6f4675841
m19e <nexus.me7290@gmail.com>
2021-01-16 04:56:23 +0900

    Add novel index page

---
+++ pages/novel/[id]/index.tsx
@@ -0,0 +1,49 @@
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import firebase from "firebase/app";
+import NovelViewer from "../../../components/NovelViewer";
+import { INovelDataSerializable, getAllNovelIDs, getNovel } from "../../../lib/firebase/initFirebase";
+
+export default function NovelIndex({ novel }: { novel: INovelDataSerializable }) {
+    return <NovelViewer content={novel.content} />;
+}
+
+const getDisplayTime = (milli: number): string => {
+    const dt = new Date(milli);
+    const y = dt.getFullYear() + "年";
+    const m = dt.getMonth() + 1 + "月";
+    const d = dt.getDate() + "日 ";
+    const ho = ("00" + dt.getHours()).slice(-2) + ":";
+    const mi = ("00" + dt.getMinutes()).slice(-2);
+    return y + m + d + ho + mi;
+};
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+    const novel = await getNovel(params.id);
+    const update = {
+        created_at: getDisplayTime((novel.created_at as firebase.firestore.Timestamp).toMillis()),
+        updated_at: getDisplayTime((novel.updated_at as firebase.firestore.Timestamp).toMillis()),
+    };
+    const serializable: INovelDataSerializable = Object.assign(novel, update);
+
+    return {
+        props: {
+            novel: serializable,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const ids = await getAllNovelIDs();
+
+    return {
+        paths: ids.map((id) => {
+            return {
+                params: {
+                    id,
+                },
+            };
+        }),
+        fallback: false,
+    };
+}
