b5157aabdfdee3bd11b085f43d88053c63a88ccd
m19e <nexus.me7290@gmail.com>
2021-01-16 00:35:12 +0900

    Remove transition when show modal

--- components/EditorFrame.tsx
+++ components/EditorFrame.tsx
@@ -1,13 +1,13 @@
-import { useIsMincho } from "../store/editor";
+import { useIsMincho, useIsShowPublishModal } from "../store/editor";
 import Header from "./EditorHeader";
 import Footer from "./EditorFooter";
 
 export default function Frame() {
     const [isMincho] = useIsMincho();
+    const [isShowModal] = useIsShowPublishModal();
 
     return (
-        <div className={"transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100" + (isMincho ? " mincho" : " gothic")}>
-            {/* <div className={"opacity-100" + (isMincho ? " mincho" : " gothic")}> */}
+        <div className={(isShowModal ? "" : "transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100") + (isMincho ? " mincho" : " gothic")}>
             <Footer />
             <Header />
         </div>
