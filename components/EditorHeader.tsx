917f1e3dd0dcc49119c8922868cf3e5fc097a6c8
m19e <nexus.me7290@gmail.com>
2021-01-16 00:37:40 +0900

    Adjust header icon size

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -53,7 +53,7 @@ export default function Header() {
                         <span className="px-4 opacity-50">
                             <Link href="/">
                                 <a>
-                                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                                     </svg>
                                 </a>
