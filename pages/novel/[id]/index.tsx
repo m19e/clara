3be302647a4a94816cd00876b2a0a15c7a4ab963
m19e <nexus.me7290@gmail.com>
2021-01-16 05:14:42 +0900

    Update created_at date format

--- pages/novel/[id]/index.tsx
+++ pages/novel/[id]/index.tsx
@@ -10,9 +10,9 @@ export default function NovelIndex({ novel }: { novel: INovelDataSerializable })
 
 const getDisplayTime = (milli: number): string => {
     const dt = new Date(milli);
-    const y = dt.getFullYear() + "年";
-    const m = dt.getMonth() + 1 + "月";
-    const d = dt.getDate() + "日 ";
+    const y = dt.getFullYear() + "/";
+    const m = dt.getMonth() + 1 + "/";
+    const d = dt.getDate() + " ";
     const ho = ("00" + dt.getHours()).slice(-2) + ":";
     const mi = ("00" + dt.getMinutes()).slice(-2);
     return y + m + d + ho + mi;
