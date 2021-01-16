fc92e9519f996000377191c4850d3e7d5273829b
m19e <nexus.me7290@gmail.com>
2021-01-16 10:56:34 +0900

    Adjust title opacity

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -76,7 +76,7 @@ export default function Header() {
                     ) : (
                         <>
                             <div className="w-6"></div>
-                            <p className="text-black opacity-60 mx-1">{title}</p>
+                            <p className="text-black opacity-75 mx-1">{title}</p>
                             <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-60" onClick={toggleTitleEdit}>
                                 <svg
                                     className="w-4 h-4 opacity-50 hover:opacity-100"
