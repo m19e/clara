5d555e21c6d0992e008cf013a60fa0a139804d53
m19e <nexus.me7290@gmail.com>
2021-01-18 06:28:03 +0900

    Inject data

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import { Editor, EditorState, ContentState } from "draft-js";
+import Scrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
+
 export default function NovelEditor({ title, content }: { title: string; content: string }) {
+    const [editorState, setEditorState] = useState(EditorState.createWithContent(ContentState.createFromText(content)));
+
     return (
-        <div>
-            <h1>{title}</h1>
-            <h2>{content}</h2>
+        <div className="h-screen w-full flex-center editor-bg">
+            <Scrollbar className="h-full w-full flex items-center">
+                <div className="px-6 writing-v-rl text-justify mincho" style={{ height: "672px", maxHeight: "75vh", minHeight: "504px", fontSize: "21px" }}>
+                    <Editor editorState={editorState} onChange={(_) => null} readOnly />
+                </div>
+            </Scrollbar>
         </div>
     );
 }
