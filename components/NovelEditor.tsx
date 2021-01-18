883a8ead9b20b23580cef83b9db656d31da90ad9
m19e <nexus.me7290@gmail.com>
2021-01-19 02:17:08 +0900

    Style edit page

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -22,17 +22,16 @@ export default function NovelEditor({ title, content }: { title: string; content
     }, []);
 
     return (
-        <div ref={editorRef} className="h-screen w-full flex-center editor-bg">
-            <Scrollbar className="h-full w-full flex items-center">
-                <div className="writing-v-rl">
-                    <div className="h-full p-16 mx-16 gothic border-solid border-t border-b border-gray-300">
-                        <p className="text-4xl font-bold opacity-75">{title}</p>
-                    </div>
-                    <div
-                        className="leading-relaxed text-justify pl-16 mincho"
-                        style={{ height: `${editorHeight}px`, maxHeight: "720px", minHeight: "480px", fontSize: "24px" }}
-                    >
-                        <Editor editorState={editorState} onChange={(_) => null} readOnly />
+        <div ref={editorRef} className="w-full h-screen editor-bg">
+            <Scrollbar className="w-full h-full">
+                <div className="writing-v-rl h-full flex flex-col items-center">
+                    <div style={{ height: `${editorHeight}px`, maxHeight: "720px", minHeight: "480px" }}>
+                        <div className="h-full p-16 mx-16 gothic border-solid border-t border-b border-gray-300">
+                            <p className="text-4xl font-bold opacity-75">{title}</p>
+                        </div>
+                        <div className="leading-relaxed text-justify pl-16 mincho" style={{ fontSize: "24px" }}>
+                            <Editor editorState={editorState} onChange={setEditorState} />
+                        </div>
                     </div>
                 </div>
             </Scrollbar>
