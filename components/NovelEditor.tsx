f3b1b23b2524f77f197ac64c33e6f2df8d357bfa
m19e <nexus.me7290@gmail.com>
2021-01-19 07:27:50 +0900

    Set title edit button

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -27,7 +27,19 @@ export default function NovelEditor({ title, content }: { title: string; content
                 <div className="writing-v-rl h-full flex flex-col items-center">
                     <div style={{ height: `${editorHeight}px`, maxHeight: "720px", minHeight: "480px" }}>
                         <div className="h-full p-16 mx-16 gothic border-solid border-t border-b border-gray-300">
-                            <p className="text-4xl font-bold opacity-75">{title}</p>
+                            <div className="flex items-center">
+                                <span className="text-4xl font-bold opacity-75">{title}</span>
+                                <span className="mt-1 w-full rounded bg-gray-300 transition-opacity opacity-50 hover:opacity-60">
+                                    <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                        />
+                                    </svg>
+                                </span>
+                            </div>
                         </div>
                         <div className="leading-relaxed text-justify pl-16 mincho" style={{ fontSize: "24px" }}>
                             <Editor editorState={editorState} onChange={setEditorState} />
