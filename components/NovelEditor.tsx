4c203e51f8d4a4214a05e7906ab326976320bbc5
m19e <nexus.me7290@gmail.com>
2021-01-19 08:57:00 +0900

    Add novel edit toolbar

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -47,6 +47,42 @@ export default function NovelEditor({ title, content }: { title: string; content
                     </div>
                 </div>
             </Scrollbar>
+            <div className={"fixed bottom-0 w-12 mb-4 editor-bg border border-r-0 border-solid border-gray-300 rounded-l-lg novelView-header__show"}>
+                <div className="flex-col flex-center w-full my-3">
+                    <svg
+                        className="w-full h-8 transition opacity-50 hover:opacity-70 cursor-pointer"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                    </svg>
+                    <svg
+                        className="w-full h-8 transition opacity-50 hover:opacity-70 cursor-pointer"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    <svg
+                        className="w-full h-8 transition opacity-50 hover:opacity-70 cursor-pointer"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                        />
+                    </svg>
+                </div>
+            </div>
         </div>
     );
 }
