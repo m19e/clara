50f32bb9166debeb12f7eadf183e54caa5a7b5d2
m19e <nexus.me7290@gmail.com>
2021-01-20 02:59:23 +0900

    Add span delete button and others

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -114,8 +114,9 @@ export default function NovelEditor({ title, content }: { title: string; content
                     >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
+                    <NovelViewerConfig viewerConfig={viewerConfig} />
                     <svg
-                        className="w-full h-8 transition opacity-50 hover:opacity-70 cursor-pointer"
+                        className="w-full h-8 mt-8 transition opacity-50 hover:opacity-70 cursor-pointer"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -128,7 +129,6 @@ export default function NovelEditor({ title, content }: { title: string; content
                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                         />
                     </svg>
-                    <NovelViewerConfig viewerConfig={viewerConfig} />
                 </div>
             </div>
         </div>
