a1a263e114378d410554d29fba8a390a028ce040
m19e <nexus.me7290@gmail.com>
2021-01-20 04:39:48 +0900

    Set tooltip test

--- components/NovelEditor.tsx
+++ components/NovelEditor.tsx
@@ -4,6 +4,7 @@ import Scrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 
 import NovelViewerConfig from "./NovelViewerConfig";
+import Tooltip from "./Tooltip";
 
 const useFontSize = (fs: "base" | "xl" | "2xl"): ["base" | "xl" | "2xl", () => void, () => void, () => void] => {
     const [fontSize, setFontSize] = useState(fs);
@@ -129,6 +130,7 @@ export default function NovelEditor({ title, content }: { title: string; content
                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                         />
                     </svg>
+                    <Tooltip d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                 </div>
             </div>
         </div>
