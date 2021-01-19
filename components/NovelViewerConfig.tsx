7f0fa9eb9e73add9279606be4774b3abeccd05fd
m19e <nexus.me7290@gmail.com>
2021-01-20 06:08:03 +0900

    Fix onclick

--- components/NovelViewerConfig.tsx
+++ components/NovelViewerConfig.tsx
@@ -19,10 +19,12 @@ export default function NovelViewConfig({ viewerConfig }: { viewerConfig: Viewer
 
     return (
         <>
-            <Tooltip
-                text="書式設定"
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-            />
+            <div onClick={() => setShowModal(true)}>
+                <Tooltip
+                    text="書式設定"
+                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                />
+            </div>
             {showModal && (
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
