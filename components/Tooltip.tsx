a3ac6271fed8a194ab2a71cd811878691b0b07b7
m19e <nexus.me7290@gmail.com>
2021-01-24 00:48:32 +0900

    Set circle button with no border

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -13,7 +13,7 @@ type TooltipProps = {
 export default function Tooltip({
     text,
     d,
-    classOverride = "w-full h-8 transition-opacity opacity-50 hover:opacity-70 cursor-pointer",
+    classOverride = "w-8 h-8 transition-opacity opacity-50 hover:opacity-70 cursor-pointer",
     fill = "none",
     stroke = "currentColor",
     viewBox = "0 0 24 24",
@@ -33,20 +33,22 @@ export default function Tooltip({
 
     return (
         <>
-            <svg
-                className={classOverride}
-                xmlns="http://www.w3.org/2000/svg"
-                fill={fill}
-                viewBox={viewBox}
-                stroke={stroke}
-                onMouseEnter={openLeftTooltip}
-                onMouseLeave={closeLeftTooltip}
-                ref={btnRef}
-            >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={d} />
-            </svg>
+            <div className="w-10 h-10 mt-2 editor-bg rounded-full shadow-md flex-center">
+                <svg
+                    className={classOverride}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill={fill}
+                    viewBox={viewBox}
+                    stroke={stroke}
+                    onMouseEnter={openLeftTooltip}
+                    onMouseLeave={closeLeftTooltip}
+                    ref={btnRef}
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={d} />
+                </svg>
+            </div>
             <div
-                className={(tooltipShow ? "" : "hidden ") + "editor-bg mr-2 block z-50 no-underline break-words rounded-xl border border-solid border-gray-300"}
+                className={(tooltipShow ? "" : "hidden ") + "editor-bg mr-4 block z-50 no-underline break-words rounded-xl border border-solid border-gray-300"}
                 ref={tooltipRef}
             >
                 <div className="writing-v-rl opacity-70 font-semibold p-3 mb-0">
