1f9e5e747bf093c86a3434a81e5c01b9ead9d6f1
m19e <nexus.me7290@gmail.com>
2021-01-24 01:07:17 +0900

    Remove commented markup

--- components/NovelViewer.tsx
+++ components/NovelViewer.tsx
@@ -99,15 +99,6 @@ export default function NovelView({ novel }: { novel: INovelDataSerializable }) 
             </Scrollbar>
             <div className={"fixed bottom-0 w-12 mb-4 mr-2 novelView-header" + (show ? " novelView-header__show" : "")}>
                 <div className="flex-col flex-center w-full">
-                    {/* <svg
-                        className="w-full h-8 transition opacity-50 hover:opacity-70"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                    </svg> */}
                     {isAuthor && (
                         <Link href={`/novel/${novel.id}/edit`}>
                             <a>
@@ -119,20 +110,6 @@ export default function NovelView({ novel }: { novel: INovelDataSerializable }) 
                         </Link>
                     )}
                     <NovelViewerConfig viewerConfig={viewerConfig} />
-                    {/* <svg
-                        className="w-full h-8 mt-2 transition opacity-50 hover:opacity-70"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                        />
-                    </svg> */}
                     <a
                         href={
                             "https://twitter.com/intent/tweet?text=" +
