1ac30c82cba5a0dcad8b686f3cf93aa2eb0124ce
m19e <nexus.me7290@gmail.com>
2021-01-23 22:50:28 +0900

    Spacing modal, adjust border

--- components/ConfirmableModal.tsx
+++ components/ConfirmableModal.tsx
@@ -32,26 +32,26 @@ export default function ConfirmableModal({ popperText, d, message, confirmText, 
             {showModal && (
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative w-auto my-6 mx-auto max-w-md">
+                        <div className="relative w-auto my-6 mx-auto max-w-xs">
                             <div className="gothic border-0 rounded-lg shadow-lg relative flex flex-col w-full p-6 pb-4 bg-white outline-none focus:outline-none">
                                 <div className="flex-center">{message}</div>
                                 <div className="flex-center">
-                                    <div className="flex justify-between w-72 opacity-80 mt-10">
+                                    <div className="flex justify-between w-72 opacity-80 mt-12">
                                         <span
-                                            className="w-28 py-1 text-center font-semibold opacity-75 editor-bg border border-solid border-gray-300 rounded-3xl cursor-pointer"
+                                            className="w-20 text-center font-semibold opacity-75 editor-bg border-b border-solid border-gray-300 cursor-pointer"
                                             onClick={() => setShowModal(false)}
                                         >
                                             {cancelText}
                                         </span>
                                         {back ? (
                                             <Link href={`/novel/${novelID}`}>
-                                                <a className="w-28 py-1 text-center font-semibold opacity-75 editor-bg border border-solid border-gray-300 rounded-3xl cursor-pointer">
+                                                <a className="w-20 text-center font-semibold opacity-75 editor-bg border-b border-solid border-gray-300 cursor-pointer">
                                                     {confirmText}
                                                 </a>
                                             </Link>
                                         ) : (
                                             <span
-                                                className="w-28 py-1 text-center font-semibold opacity-75 editor-bg border border-solid border-gray-300 rounded-3xl cursor-pointer"
+                                                className="w-20 text-center font-semibold opacity-75 editor-bg border-b border-solid border-gray-300 cursor-pointer"
                                                 onClick={() => execConfirm()}
                                             >
                                                 {confirmText}
