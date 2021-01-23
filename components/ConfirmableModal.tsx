304e088b2c98459b9d0ca200a1475265ccc5c98e
m19e <nexus.me7290@gmail.com>
2021-01-23 22:55:05 +0900

    Adjust modal rounded

--- components/ConfirmableModal.tsx
+++ components/ConfirmableModal.tsx
@@ -33,7 +33,7 @@ export default function ConfirmableModal({ popperText, d, message, confirmText, 
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-xs">
-                            <div className="gothic border-0 rounded-lg shadow-lg relative flex flex-col w-full p-6 pb-4 bg-white outline-none focus:outline-none">
+                            <div className="gothic border-0 rounded shadow-lg relative flex flex-col w-full p-6 pb-4 bg-white outline-none focus:outline-none">
                                 <div className="flex-center">{message}</div>
                                 <div className="flex-center">
                                     <div className="flex justify-between w-72 opacity-80 mt-12">
--- components/NovelTitleEditModal.tsx
+++ components/NovelTitleEditModal.tsx
@@ -55,7 +55,7 @@ export default function TitleEditModal({ title, setTitle }: { title: string; set
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-md">
-                            <div className="gothic border-0 rounded-lg shadow-lg relative flex flex-col w-full p-6 pb-4 bg-white outline-none focus:outline-none">
+                            <div className="gothic border-0 rounded shadow-lg relative flex flex-col w-full p-6 pb-4 bg-white outline-none focus:outline-none">
                                 <input
                                     type="text"
                                     className="w-72 py-1 px-2 editor-bg gothic text-xl text-center rounded shadow-inner outline-none focus:outline-none"
