973d62f81a94d1c073655b0a27ecabd83b425914
m19e <nexus.me7290@gmail.com>
2021-01-25 00:14:21 +0900

    Apply responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Layout from "../components/Layout";
 
 export default function Top() {
-    const [rootList] = useState([...Array(13)]);
+    const [rootList] = useState([...Array(100)]);
     const [displayList, setDisplayList] = useState(rootList.slice(0, 10));
     const [hasMore, setHasMore] = useState(rootList.length > 10);
 
@@ -19,9 +19,9 @@ export default function Top() {
         <Layout>
             <div className="flex-center">
                 <div className="w-full flex flex-col flex-center mb-16">
-                    <div className="flex justify-center flex-wrap items-end w-3/4 max-w-7xl">
+                    <div className="flex justify-center flex-wrap items-end container">
                         {displayList.map((_, i) => (
-                            <div className="w-full max-w-xl mt-8 mx-4 border-b border-solid border-gray-300">
+                            <div className="w-3/4 mt-8 xl:max-w-lg xl:mx-4 2xl:max-w-xl border-b border-solid border-gray-300">
                                 <p className="text-2xl font-semibold whitespace-pre-wrap opacity-75">小説{i + 1}のタイトル</p>
                                 <div className="flex justify-between mt-1 items-baseline">
                                     <p className="opacity-75">小説の作者</p>
