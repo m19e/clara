c872e8a6a46a3529ec726fb555fcd2408f940d32
m19e <nexus.me7290@gmail.com>
2021-01-25 01:08:21 +0900

    Set content bg-color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 export default function Top() {
     const [rootList] = useState([...Array(100)]);
-    const [displayList, setDisplayList] = useState(rootList.slice(0, 10));
+    const [displayList, setDisplayList] = useState(rootList.slice(0, 20));
     const [hasMore, setHasMore] = useState(rootList.length > 10);
 
     const displayMore = () => {
@@ -18,24 +18,28 @@ export default function Top() {
     return (
         <Layout>
             <div className="flex-center">
-                <div className="w-full flex flex-col flex-center mb-16">
-                    <div className="flex justify-center flex-wrap items-end container">
-                        {displayList.map((_, i) => (
-                            <div className="w-3/4 mt-8 xl:max-w-lg xl:mx-4 2xl:max-w-xl border-b border-solid border-gray-300">
-                                <p className="text-2xl font-semibold whitespace-pre-wrap opacity-75">小説{i + 1}のタイトル</p>
-                                <div className="flex justify-between mt-1 items-baseline">
-                                    <p className="opacity-75">小説の作者</p>
-                                    <p className="text-sm opacity-50">小説の文字数</p>
+                <div className="w-full flex flex-col flex-center mt-4 mb-8">
+                    <div className="container flex-center">
+                        <div className="w-11/12 flex justify-center flex-wrap items-end editor-bg">
+                            {displayList.map((_, i) => (
+                                <div className="w-3/4 mt-8 xl:max-w-lg xl:mx-4 2xl:max-w-xl border-b border-solid border-gray-300">
+                                    <p className="text-2xl font-semibold whitespace-pre-wrap opacity-75">小説{i + 1}のタイトル</p>
+                                    <div className="flex justify-between mt-1 items-baseline">
+                                        <p className="opacity-75">小説の作者</p>
+                                        <p className="text-sm opacity-50">小説の文字数</p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                        <div className="w-full max-w-xl mx-4"></div>
+                            ))}
+                            <div className="w-full max-w-xl mx-4"></div>
+                            {hasMore && (
+                                <div className="w-full flex-center my-8 editor-bg">
+                                    <button className="p-2 border border-solid border-gray-300" onClick={() => displayMore()}>
+                                        Load More
+                                    </button>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    {hasMore && (
-                        <button className="my-4 p-2 border border-solid border-gray-300" onClick={() => displayMore()}>
-                            Load More
-                        </button>
-                    )}
                 </div>
             </div>
         </Layout>
