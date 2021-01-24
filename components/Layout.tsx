66ffc4ea87fc082c8d60c15bda83b69725afc3c3
m19e <nexus.me7290@gmail.com>
2021-01-24 14:03:41 +0900

    Recieve children component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,17 @@
 import JumpEditorButton from "./JumpEditorButton";
 
-export default function Layout() {
+export default function Layout({ children }) {
     return (
         <div className="min-h-screen w-full bg-gray-200">
             <nav className="h-16 w-full flex-center editor-bg">
-                <div className="container flex flex-wrap items-center justify-between">
-                    <h1 className="mincho font-black text-3xl text-gray-600 mx-4">Clara</h1>
-                    <JumpEditorButton />
+                <div className="w-3/4">
+                    <div className="flex flex-wrap items-center justify-between">
+                        <h1 className="mincho font-black text-3xl text-gray-600 mx-4">Clara</h1>
+                        <JumpEditorButton />
+                    </div>
                 </div>
             </nav>
+            {children}
         </div>
     );
 }
