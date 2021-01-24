fd042f1f433248b20e2f11b3d37de1ca29a92269
m19e <nexus.me7290@gmail.com>
2021-01-24 19:25:12 +0900

    Fit page content width

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <div className="min-h-screen w-full bg-gray-200">
             <nav className="h-16 w-full flex-center editor-bg">
-                <div className="w-3/4">
+                <div className="w-3/4 max-w-7xl">
                     <div className="flex flex-wrap items-center justify-between">
                         <h1 className="mincho font-black text-3xl text-gray-600 mx-4">Clara</h1>
                         <JumpEditorButton />
