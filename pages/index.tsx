d5b5d730af28a63ec3aafeae76c0f90f654b5a58
m19e <nexus.me7290@gmail.com>
2021-01-24 19:25:39 +0900

    Demo novel list view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
 export default function Top() {
-    return <Layout />;
+    const [rootList] = useState([...Array(13)]);
+    const [displayList, setDisplayList] = useState(rootList.slice(0, 10));
+    const [hasMore, setHasMore] = useState(rootList.length > 10);
+
+    const displayMore = () => {
+        const displayListLen = displayList.length;
+        const moreItems = rootList.slice(0, displayListLen + 10);
+        if (moreItems.length === rootList.length) {
+            setHasMore(false);
+        }
+        setDisplayList(moreItems);
+    };
+
+    return (
+        <Layout>
+            <div className="flex-center">
+                <div className="w-full flex flex-col flex-center mb-16">
+                    <div className="flex justify-center flex-wrap items-end w-3/4 max-w-7xl">
+                        {displayList.map((_, i) => (
+                            <div className="w-full max-w-xl mt-8 mx-4 border-b border-solid border-gray-300">
+                                <p className="text-2xl font-semibold whitespace-pre-wrap opacity-75">小説{i + 1}のタイトル</p>
+                                <div className="flex justify-between mt-1 items-baseline">
+                                    <p className="opacity-75">小説の作者</p>
+                                    <p className="text-sm opacity-50">小説の文字数</p>
+                                </div>
+                            </div>
+                        ))}
+                        <div className="w-full max-w-xl mx-4"></div>
+                    </div>
+                    {hasMore && (
+                        <button className="my-4 p-2 border border-solid border-gray-300" onClick={() => displayMore()}>
+                            Load More
+                        </button>
+                    )}
+                </div>
+            </div>
+        </Layout>
+    );
 }
