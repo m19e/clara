7183d3529974e8175f46e7bdc54c9af9473acfa6
m19e <nexus.me7290@gmail.com>
2021-01-28 00:13:47 +0900

    Set top page link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import JumpEditorButton from "./JumpEditorButton";
 
 export default function Layout({ children }) {
@@ -6,7 +7,9 @@ export default function Layout({ children }) {
             <nav className="h-16 w-full flex-center editor-bg">
                 <div className="container flex-center">
                     <div className="w-11/12 flex flex-wrap items-center justify-between">
-                        <h1 className="mincho font-black text-3xl text-gray-600">Clara</h1>
+                        <Link href="/">
+                            <a className="mincho font-black text-3xl text-gray-600">Clara</a>
+                        </Link>
                         <JumpEditorButton />
                     </div>
                 </div>
