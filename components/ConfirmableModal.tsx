a2ac02feec2d88b8d23859d9b4c6efa2666568d9
m19e <nexus.me7290@gmail.com>
2021-01-29 06:10:26 +0900

    Click outside modal, close modal

--- components/ConfirmableModal.tsx
+++ components/ConfirmableModal.tsx
@@ -31,9 +31,15 @@ export default function ConfirmableModal({ popperText, d, message, confirmText, 
             </div>
             {showModal && (
                 <>
-                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        onClick={() => setShowModal(false)}
+                    >
                         <div className="relative w-auto my-6 mx-auto max-w-xs">
-                            <div className="gothic border-0 rounded shadow-lg relative flex flex-col w-full p-6 pb-4 editor-bg outline-none focus:outline-none">
+                            <div
+                                className="gothic border-0 rounded shadow-lg relative flex flex-col w-full p-6 pb-4 editor-bg outline-none focus:outline-none"
+                                onClick={(e) => e.stopPropagation()}
+                            >
                                 <div className="flex-center text-lg">{message}</div>
                                 <div className="flex-center">
                                     <div className="flex justify-between w-72 opacity-80 mt-12">
