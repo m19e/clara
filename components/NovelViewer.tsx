79a95fd5b5bdb651ebc7a61ac9159498ce11baee
m19e <nexus.me7290@gmail.com>
2021-01-28 11:50:40 +0900

    Set user page link

--- components/NovelViewer.tsx
+++ components/NovelViewer.tsx
@@ -85,7 +85,11 @@ export default function NovelView({ novel }: { novel: INovelDataSerializable }) 
                         <div className="h-full p-16 mx-16 gothic border-solid border-t border-b border-gray-300">
                             <p className="text-sm pt-1 opacity-50">{novel.created_at}</p>
                             <p className="text-4xl font-bold whitespace-pre-wrap opacity-75">{novel.title}</p>
-                            <p className="text-xl font-semibold pt-1 opacity-50">{novel.author_name}</p>
+                            <div className="pt-1">
+                                <Link href={`/user/${novel.author_id}`}>
+                                    <a className="text-xl font-semibold opacity-50">{novel.author_name}</a>
+                                </Link>
+                            </div>
                         </div>
                         <div className={"leading-relaxed text-justify pl-16 text-" + fontSize + " " + font}>
                             {contentArray.map((line, i) => (
