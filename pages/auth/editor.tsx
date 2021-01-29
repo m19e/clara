d948a1d5242e010ee494e6c86a0233b7db58725d
m19e <nexus.me7290@gmail.com>
2021-01-29 23:44:59 +0900

    Remove needless pages

--- pages/auth/editor.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const DynamicStoreEditor = dynamic(() => import("../../components/EditorStore"), { ssr: false });
-
-export default function StoreEditorIndex() {
-    return <DynamicStoreEditor />;
-}
