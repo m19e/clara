2235f63db9747f0c325132846e37dbe5c6f35df6
m19e <nexus.me7290@gmail.com>
2021-01-30 03:51:28 +0900

    Display frame if isshowmodal OR istitleedit

--- components/EditorFrame.tsx
+++ components/EditorFrame.tsx
@@ -1,13 +1,19 @@
 import { useIsMincho, useIsShowPublishModal } from "../store/editor";
+import { useIsTitleEdit } from "../store/draft";
 import Header from "./EditorHeader";
 import Footer from "./EditorFooter";
 
 export default function Frame() {
     const [isMincho] = useIsMincho();
     const [isShowModal] = useIsShowPublishModal();
+    const [isTitleEdit] = useIsTitleEdit();
 
     return (
-        <div className={(isShowModal ? "" : "transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100") + (isMincho ? " mincho" : " gothic")}>
+        <div
+            className={
+                (isShowModal || isTitleEdit ? "" : "transition-opacity duration-1000 ease-out opacity-0 hover:opacity-100") + (isMincho ? " mincho" : " gothic")
+            }
+        >
             <Footer />
             <Header />
         </div>
