2f0e634103afc49964c92b4ae975f86de19d63d3
m19e <nexus.me7290@gmail.com>
2021-01-31 05:06:07 +0900

    Remove router

--- components/JumpEditorButton.tsx
+++ components/JumpEditorButton.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { auth, loginWithTwitter } from "../lib/firebase/initFirebase";
 
 export default function JumpEditorButton() {
-    const router = useRouter();
     const [showModal, setShowModal] = useState(false);
     const [isLoggedin, setIsLoggedin] = useState(false);
 
