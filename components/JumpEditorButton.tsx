552429d14e8b0cc392b0c72c7521b632812f80bb
m19e <nexus.me7290@gmail.com>
2021-01-31 05:05:57 +0900

    Link /editor when loggedin

--- components/JumpEditorButton.tsx
+++ components/JumpEditorButton.tsx
@@ -1,30 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { auth, loginWithTwitter } from "../lib/firebase/initFirebase";
 
 export default function JumpEditorButton() {
     const router = useRouter();
     const [showModal, setShowModal] = useState(false);
+    const [isLoggedin, setIsLoggedin] = useState(false);
 
-    const checkProfile = () => {
+    useEffect(() => {
         auth.onAuthStateChanged((user) => {
-            user ? router.push("/editor") : setShowModal(true);
+            setIsLoggedin(!!user);
         });
-    };
+    }, []);
 
     return (
         <>
-            <div className="flex-center px-6 bg-gray-200 bg-opacity-60 rounded-2xl cursor-pointer" onClick={() => checkProfile()}>
-                <span className="gothic font-black text-sm opacity-60 py-2">小説を書く</span>
-                <svg className="w-4 h-4 opacity-50 hover:opacity-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                    />
-                </svg>
-            </div>
+            {isLoggedin ? (
+                <Link href="/editor">
+                    <a className="flex-center px-6 bg-gray-200 bg-opacity-60 rounded-2xl cursor-pointer">
+                        <span className="gothic font-black text-sm opacity-60 py-2">小説を書く</span>
+                        <svg
+                            className="w-4 h-4 opacity-50 hover:opacity-100"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                            />
+                        </svg>
+                    </a>
+                </Link>
+            ) : (
+                <div className="flex-center px-6 bg-gray-200 bg-opacity-60 rounded-2xl cursor-pointer" onClick={() => setShowModal(true)}>
+                    <span className="gothic font-black text-sm opacity-60 py-2">小説を書く</span>
+                    <svg
+                        className="w-4 h-4 opacity-50 hover:opacity-100"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                        />
+                    </svg>
+                </div>
+            )}
             {showModal && (
                 <>
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
