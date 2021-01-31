e100e693ef9fc28830fcccb92519f70495df41af
m19e <nexus.me7290@gmail.com>
2021-01-31 23:20:34 +0900

    Set guest icon

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -24,13 +24,30 @@ export default function UserMenu({ user }: { user: User | null }) {
             <div className="w-full">
                 <div className="relative inline-flex align-middle w-full">
                     <div
-                        className="w-9 h-9 ml-2 rounded-full bg-gray-200"
+                        className="w-9 h-9 ml-2 flex-center rounded-full bg-gray-200"
                         ref={btnDropdownRef}
                         onClick={() => {
                             dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
                         }}
                     >
-                        {user && <img className="w-full h-full rounded-full" src={user.photoURL.replace(/.jpg/, "_normal.jpg")} />}
+                        {user ? (
+                            <img className="w-full h-full rounded-full" src={user.photoURL.replace(/.jpg/, "_normal.jpg")} />
+                        ) : (
+                            <svg
+                                className="w-6 h-6 text-gray-400"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                />
+                            </svg>
+                        )}
                     </div>
                     <div
                         ref={popoverDropdownRef}
