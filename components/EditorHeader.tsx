bf0c4e221154e477e44f3b4d41d8b123708d57cb
m19e <nexus.me7290@gmail.com>
2021-02-01 03:47:34 +0900

    Use home icon instead of back arrow

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -53,7 +53,12 @@ export default function Header() {
                             <Link href="/">
                                 <a>
                                     <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                                        />
                                     </svg>
                                 </a>
                             </Link>
