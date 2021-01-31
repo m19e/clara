3ac57e42a381e15fc05d49439c5e7a7b3b01f1da
m19e <nexus.me7290@gmail.com>
2021-01-31 23:58:08 +0900

    Set width menu

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -60,8 +60,9 @@ export default function UserMenu({ user }: { user: User | null }) {
                     </div>
                     <div
                         ref={popoverDropdownRef}
-                        className={(dropdownPopoverShow ? "block " : "hidden ") + "text-base z-50 list-none text-left rounded shadow-md editor-bg mt-5 -ml-36"}
-                        style={{ minWidth: "12rem" }}
+                        className={
+                            (dropdownPopoverShow ? "block " : "hidden ") + "text-base w-48 z-50 list-none text-left rounded shadow-md editor-bg mt-5 -ml-36"
+                        }
                     >
                         {user && (
                             <>
