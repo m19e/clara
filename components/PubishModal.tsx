b4718fc6debcf1fe37dd8dcfe0351fc01ad167f2
m19e <nexus.me7290@gmail.com>
2021-02-01 03:45:54 +0900

    Adjust size header button

--- components/PubishModal.tsx
+++ components/PubishModal.tsx
@@ -35,7 +35,7 @@ export default function PublishModal() {
     return (
         <>
             <svg
-                className="w-5 h-5 opacity-50"
+                className="w-6 h-6 opacity-50"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 width="24"
