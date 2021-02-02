2b92803690fb0a58fa17f394e5c1b8a236f22615
m19e <nexus.me7290@gmail.com>
2021-02-02 14:56:04 +0900

    Set icon home button

--- components/EditorHeader.tsx
+++ components/EditorHeader.tsx
@@ -49,17 +49,10 @@ export default function Header() {
             <div className="container flex justify-between items-center">
                 <div className="w-20">
                     <div className="flex justify-start items-center">
-                        <span className="px-4 opacity-50">
+                        <span className="px-4">
                             <Link href="/">
                                 <a>
-                                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                        />
-                                    </svg>
+                                    <img className="w-6 h-6" src="/icon-64x64.png" alt="Clara" />
                                 </a>
                             </Link>
                         </span>
