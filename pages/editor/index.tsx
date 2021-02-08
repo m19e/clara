e8c8395e55097510d9097c95594f1ede2978f01a
m19e <nexus.me7290@gmail.com>
2021-02-09 04:18:15 +0900

    Check mobile access in editor page

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,7 +1,25 @@
 import dynamic from "next/dynamic";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useUserAgent, UserAgent } from "next-useragent";
+import { useRouter } from "next/router";
 
 const DynamicEditor = dynamic(() => import("../../components/DraftEditor"), { ssr: false });
 
-export default function EditorIndex() {
+export default function EditorIndex({ ua }: { ua: UserAgent }) {
+    const router = useRouter();
+    if (ua.isMobile) {
+        router.back();
+    }
+
     return <DynamicEditor />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }: GetServerSidePropsContext) => {
+    const ua = useUserAgent(req.headers["user-agent"]);
+
+    return {
+        props: {
+            ua,
+        },
+    };
+};
