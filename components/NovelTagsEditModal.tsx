dae1088d8c8d5ed7d1bd4240abc5d312e6d4a0c3
m19e <nexus.me7290@gmail.com>
2021-02-20 11:05:08 +0900

    Add type for tags edit

--- components/NovelTagsEditModal.tsx
+++ components/NovelTagsEditModal.tsx
@@ -1,3 +1,8 @@
+type TagsEditModalProps = {
+    tags: string[];
+    setTags: (tags: string[]) => void;
+};
+
 const NovelTagsEditModal = () => (
     <span className="text-gray-400 hover:text-gray-600 w-4 h-4 mb-1 mr-0.5 flex-center cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
