f1707201fc5c8e7d8e9913335dc8e52bb70fb20c
m19e <nexus.me7290@gmail.com>
2021-02-23 21:08:45 +0900

    Adjust tags spacing

--- components/NovelTags.tsx
+++ components/NovelTags.tsx
@@ -5,7 +5,7 @@ type TagsProps = {
 const NovelTags = ({ tags }: TagsProps) => (
     <>
         {tags.map((tag) => (
-            <span className="text-gray-600 text-sm leading-none border-gray-300 pb-2 mb-2 mr-1.5 border-b">{tag}</span>
+            <span className="text-gray-600 text-sm leading-none border-gray-300 pb-2 mb-2 ml-1.5 border-b">{tag}</span>
         ))}
     </>
 );
--- components/NovelTagsEditModal.tsx
+++ components/NovelTagsEditModal.tsx
@@ -27,7 +27,7 @@ const NovelTagsEditModal = ({ tags, setTags }: TagsEditModalProps) => {
 
     return (
         <div style={{ writingMode: "initial" }}>
-            <span className="text-gray-400 hover:text-gray-600 w-4 h-4 mb-1 mr-1.5 flex-center cursor-pointer" onClick={() => initModal()}>
+            <span className="text-gray-400 hover:text-gray-600 w-4 h-4 mb-1 ml-1.5 flex-center cursor-pointer" onClick={() => initModal()}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                 </svg>
