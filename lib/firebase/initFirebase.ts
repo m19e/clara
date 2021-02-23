c1a96c6d597132eb9e549b83e50897f40e31b84c
m19e <nexus.me7290@gmail.com>
2021-02-23 19:32:06 +0900

    Add tags, r18 field

--- lib/firebase/initFirebase.ts
+++ lib/firebase/initFirebase.ts
@@ -157,6 +157,8 @@ export interface INovelProp {
     id: string;
     title: string;
     content: string;
+    tags: string[];
+    r18: boolean;
     author_id: string;
     author_uid: string;
     author_name: string;
