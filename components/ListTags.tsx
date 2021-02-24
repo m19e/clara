1dd672760f0417d5f4995fc3348fb0c34087c3dd
m19e <nexus.me7290@gmail.com>
2021-02-24 17:10:20 +0900

    Check r18 flag

--- components/ListTags.tsx
+++ components/ListTags.tsx
@@ -6,12 +6,15 @@ type TagsProps = {
 
 const ListTags = ({ novel }: TagsProps) => {
     const tags = "tags" in novel ? novel.tags : [];
+    const r18 = "r18" in novel ? novel.r18 : false;
 
     return (
         <>
-            <span className="text-sm font-semibold text-red-500 mr-1.5 -mt-1" style={{ fontFamily: "sans-serif" }}>
-                <span className="tracking-tighter">R18</span>
-            </span>
+            {r18 && (
+                <span className="text-sm font-semibold text-red-500 mr-1.5 -mt-1" style={{ fontFamily: "sans-serif" }}>
+                    <span className="tracking-tighter">R18</span>
+                </span>
+            )}
             {tags.map((tag) => (
                 <span className="text-gray-600 text-sm leading-none border-gray-300 pr-2 mr-2 mb-1.5 border-r">{tag}</span>
             ))}
