5773e249c77a314246fb845268549a17e507caac
m19e <nexus.me7290@gmail.com>
2021-02-25 12:24:00 +0900

    Add tool unify used_tags

--- lib/novel/tools.ts
+++ lib/novel/tools.ts
@@ -13,3 +13,31 @@ export const getDisplayTime = (milli: number): string => {
     const mi = ("00" + dt.getMinutes()).slice(-2);
     return y + m + d + ho + mi;
 };
+
+type UsedTag = {
+    name: string;
+    count: number;
+};
+
+export const unifyUsedTags = (rootTags: UsedTag[], oldTags: string[], newTags: string[]) => {
+    const removed = oldTags.filter((tag) => !newTags.includes(tag));
+    const added = newTags.filter((tag) => !oldTags.includes(tag));
+
+    const unified = rootTags
+        .map((tagInfo) => {
+            if (removed.includes(tagInfo.name)) {
+                return Object.assign(tagInfo, { count: tagInfo.count - 1 });
+            }
+            if (added.includes(tagInfo.name)) {
+                return Object.assign(tagInfo, { count: tagInfo.count + 1 });
+            }
+            return tagInfo;
+        })
+        .filter((tagInfo) => tagInfo.count > 0);
+
+    const rootTagsNames = rootTags.map((tagInfo) => tagInfo.name);
+    const filteredNewTags = newTags.filter((tag) => !rootTagsNames.includes(tag)).map((tag) => Object.assign({}, { name: tag, count: 1 }));
+    const newRootTags = unified.concat(filteredNewTags);
+
+    return newRootTags;
+};
