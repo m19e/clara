cc3bd70959894cdfb21db770eddd070770d94d59
m19e <nexus.me7290@gmail.com>
2021-03-02 01:48:04 +0900

    Handle root novel infos (for tags)

--- lib/firebase/novel.ts
+++ lib/firebase/novel.ts
@@ -2,6 +2,10 @@ import { db, getNovel, INovelProp } from "./initFirebase";
 
 type RootNovelInfo = {
     ids: string[];
+    infos: {
+        id: string;
+        tags: string[];
+    }[];
 };
 
 // Admin
@@ -11,15 +15,26 @@ export const saveAllNovelID = async () => {
     await db.collection("root").doc("novel").set({ ids: allNovelID }, { merge: true });
 };
 
+export const saveAllNovelInfo = async () => {
+    const allNovel = await db.collection("novel").orderBy("created_at", "desc").get();
+    const allNovelInfo = allNovel.docs.map((doc) => {
+        const novel = doc.data() as INovelProp;
+        const tags = "tags" in novel ? novel.tags : [];
+        return { id: doc.id, tags };
+    });
+    await db.collection("root").doc("novel").set({ infos: allNovelInfo }, { merge: true });
+};
+
 // User
 export const getRootNovelIDs = async (): Promise<string[]> => {
     const rootNovelInfo = await db.collection("root").doc("novel").get();
-    const { ids } = rootNovelInfo.data() as RootNovelInfo;
+    const { infos } = rootNovelInfo.data() as RootNovelInfo;
+    const ids = infos.map((info) => info.id);
     return ids;
 };
 
-export const setRootNovelIDs = async (newIds: string) => {
-    await db.collection("root").doc("novel").set({ ids: newIds }, { merge: true });
+export const setRootNovelInfos = async (newInfos: { id: string; tags: string[] }[]) => {
+    await db.collection("root").doc("novel").set({ infos: newInfos }, { merge: true });
 };
 
 export const getNovelsByIDs = async (ids: string[]): Promise<INovelProp[]> => {
