0146ecc7a581f87a24203fde5d2c886215a0c638
m19e <nexus.me7290@gmail.com>
2021-03-02 17:02:03 +0900

    Pass page count from top

--- pages/page/index.tsx
+++ pages/page/index.tsx
@@ -3,15 +3,19 @@ import { getNovelsByIDs, getRootNovelIDs } from "../../lib/firebase/novel";
 import TopPage from "../../components/top/Page";
 import { INovelProp } from "../../lib/firebase/initFirebase";
 
-const Top = ({ novels }: { novels: INovelProp[] }) => <TopPage novels={novels} />;
+const PER_PAGE = 10;
+
+const Top = ({ novels, pageCount }: { novels: INovelProp[]; pageCount: number }) => <TopPage novels={novels} pageCount={pageCount} />;
 
 export const getServerSideProps: GetServerSideProps = async () => {
     const novelIDs = await getRootNovelIDs();
-    const novels = await getNovelsByIDs(novelIDs.slice(0, 10));
+    const novels = await getNovelsByIDs(novelIDs.slice(0, PER_PAGE));
+    const pageCount = Math.ceil(novelIDs.length / PER_PAGE);
 
     return {
         props: {
             novels,
+            pageCount,
         },
     };
 };
