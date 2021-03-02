fd2021a02d8eafc1d81ff62351497d136ddc1e39
m19e <nexus.me7290@gmail.com>
2021-03-02 16:20:49 +0900

    Unify margin-x

--- components/top/Pagination.tsx
+++ components/top/Pagination.tsx
@@ -3,15 +3,11 @@ import { useEffect } from "react";
 import Paginate from "react-paginate";
 
 const chevron = (left: boolean) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={"w-4 h-4 text-gray-600 " + (left ? "mr-1" : "ml-1")}
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-    >
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={left ? "M15 19l-7-7 7-7" : "M9 5l7 7-7 7"} />
-    </svg>
+    <div className="mx-1">
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={left ? "M15 19l-7-7 7-7" : "M9 5l7 7-7 7"} />
+        </svg>
+    </div>
 );
 
 const Pagination = () => {
