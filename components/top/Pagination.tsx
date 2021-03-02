f79da115d394299a202bb96947279df158f8f341
m19e <nexus.me7290@gmail.com>
2021-03-02 14:55:01 +0900

    Try paginate

--- components/top/Pagination.tsx
+++ components/top/Pagination.tsx
@@ -1,5 +1,38 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Paginate from "react-paginate";
+
 const Pagination = () => {
-    return <div>{"placehold"}</div>;
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChangeComplete = () => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        };
+
+        router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChangeComplete);
+        };
+    }, []);
+
+    const handlePageChange = (selectedItem: { selected: number }) => {
+        const { selected } = selectedItem;
+        router.push(`/page/${selected + 1}`);
+    };
+
+    return (
+        <Paginate
+            pageCount={7}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={1}
+            onPageChange={handlePageChange}
+            initialPage={0}
+            containerClassName="flex"
+            pageClassName="mx-1"
+        />
+    );
 };
 
 export default Pagination;
