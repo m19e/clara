eaf821be3c9db150dde86f3d58c81c62090af7cb
m19e <nexus.me7290@gmail.com>
2021-03-03 14:49:58 +0900

    Arrow function

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,20 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 
-export default function NotFound() {
-    return (
-        <Layout>
-            <Head>
-                <title>ページが見つかりません | Clara</title>
-            </Head>
-            <div className="flex-center" style={{ height: "calc(100vh - 4rem)" }}>
-                <div className="flex items-center">
-                    <div className="py-3 pr-5 mr-4 border-r border-gray-400">
-                        <h1 className="text-2xl font-black">404</h1>
-                    </div>
-                    <div>
-                        <span className="text-xs">ページが見つかりません</span>
-                    </div>
+const NotFound = () => (
+    <Layout>
+        <Head>
+            <title>ページが見つかりません | Clara</title>
+        </Head>
+        <div className="flex-center" style={{ height: "calc(100vh - 4rem)" }}>
+            <div className="flex items-center">
+                <div className="py-3 pr-5 mr-4 border-r border-gray-400">
+                    <h1 className="text-2xl font-black">404</h1>
                 </div>
+                <span className="text-xs">ページが見つかりません</span>
             </div>
-        </Layout>
-    );
-}
+        </div>
+    </Layout>
+);
+
+export default NotFound;
