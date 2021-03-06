48cf692148d3172dfae565e2c418088e7feb5dfe
m19e <nexus.me7290@gmail.com>
2021-03-06 13:55:37 +0900

    Display novel list

--- components/tag/Page.tsx
+++ components/tag/Page.tsx
@@ -1,3 +1,19 @@
-const TagPage = () => <div className="w-full">Tag Page</div>;
+import { INovelProp } from "../../lib/firebase/initFirebase";
+
+type Props = {
+    tag: string;
+    novels: INovelProp[];
+};
+
+const TagPage = ({ tag, novels }: Props) => (
+    <div className="w-full pl-8 flex flex-col">
+        <span className="mb-4">
+            #{tag} の小説 {novels.length}件
+        </span>
+        {novels.map((novel) => (
+            <span>{novel.title}</span>
+        ))}
+    </div>
+);
 
 export default TagPage;
