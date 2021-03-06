b936f1dfe3d3ff1b244404002d967d9e11278059
m19e <nexus.me7290@gmail.com>
2021-03-06 15:53:31 +0900

    Markup tag search page

--- components/tag/Page.tsx
+++ components/tag/Page.tsx
@@ -1,4 +1,9 @@
+import Link from "next/link";
 import { INovelProp } from "../../lib/firebase/initFirebase";
+import { getNovelsByIDs } from "../../lib/firebase/novel";
+import { getTextCharCount } from "../../lib/novel/tools";
+import Layout from "../Layout";
+import ListTags from "../ListTags";
 
 type Props = {
     tag: string;
@@ -6,14 +11,50 @@ type Props = {
 };
 
 const TagPage = ({ tag, novels }: Props) => (
-    <div className="w-full pl-8 flex flex-col">
-        <span className="mb-4">
-            #{tag} の小説 {novels.length}件
-        </span>
-        {novels.map((novel) => (
-            <span>{novel.title}</span>
-        ))}
-    </div>
+    <Layout>
+        <div className="flex-center pb-4">
+            <div className="container max-w-4xl mx-auto px-4">
+                <div className="flex flex-center my-8">
+                    <div className="flex rounded-lg overflow-hidden whitespace-nowrap h-12">
+                        <div className="h-full flex-center editor-bg">
+                            <span className="sm:text-2xl font-black sm:font-normal text-gray-600 sm:text-gray-700 px-3">{tag}</span>
+                        </div>
+                        <div className="h-full flex-center bg-gray-400">
+                            <span className="sm:text-xl text-gray-100 whitespace-nowrap px-3">{novels.length}件</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col min-w-0 break-words editor-bg w-full mb-6 px-6 rounded-lg">
+                    <div className="flex flex-col items-center justify-end">
+                        {novels.length === 0 ? (
+                            <div className="w-3/4 max-w-xl my-12">
+                                <span className="text-xl gothic font-semibold opacity-40">まだ小説は投稿されていません</span>
+                            </div>
+                        ) : (
+                            <>
+                                {novels.map((novel, i) => (
+                                    <div key={"novel-0" + i} className="w-3/4 max-w-xl mt-12 border-b border-solid border-gray-300">
+                                        <div className="flex justify-start mb-3">
+                                            <Link href={`/novel/${novel.id}`}>
+                                                <a className="text-2xl gothic font-semibold whitespace-pre-wrap text-left opacity-75">{novel.title}</a>
+                                            </Link>
+                                        </div>
+                                        <div className="whitespace-pre-wrap ml-0.5 pb-3">
+                                            <ListTags novel={novel} />
+                                        </div>
+                                        <div className="flex justify-end items-baseline">
+                                            <span className="text-sm opacity-50">{getTextCharCount(novel.content)}字</span>
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        )}
+                        <div className="w-full flex-center my-6 editor-bg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Layout>
 );
 
 export default TagPage;
