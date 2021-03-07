7492a21c8151507d2f92db707a736e6d184f43bd
m19e <nexus.me7290@gmail.com>
2021-03-07 17:53:23 +0900

    Replace by atom

--- components/ListTags.tsx
+++ components/ListTags.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import R18Label from "./atoms/R18Label";
 import { INovelProp } from "../lib/firebase/initFirebase";
 
 type TagsProps = {
@@ -11,11 +12,7 @@ const ListTags = ({ novel }: TagsProps) => {
 
     return (
         <>
-            {r18 && (
-                <span className="text-sm font-semibold text-red-500 mr-1.5 -mt-1" style={{ fontFamily: "sans-serif" }}>
-                    <span className="tracking-tighter">R18</span>
-                </span>
-            )}
+            {r18 && <R18Label className="mr-1.5 -mt-1" />}
             {tags.map((tag, i) => (
                 <Link href={`/tag/${encodeURIComponent(tag)}`}>
                     <a key={`novel-tags-0${i}`} className="text-gray-600 hover:text-blue-400 text-sm leading-none border-gray-300 pr-2 mr-2 mb-1.5 border-r">
