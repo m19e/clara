82cb87bfa7e46e27bd894be1dd26b3bed49345b6
m19e <nexus.me7290@gmail.com>
2021-03-07 23:20:04 +0900

    Add types

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface INovelProp {
+    id: string;
+    title: string;
+    content: string;
+    tags: string[];
+    r18: boolean;
+    author_id: string;
+    author_uid: string;
+    author_name: string;
+}
