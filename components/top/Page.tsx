643189863f107b9853b4f8b5c3a16be9b1c1e71e
m19e <nexus.me7290@gmail.com>
2021-03-07 19:46:08 +0900

    Replace by foundation

--- components/top/Page.tsx
+++ components/top/Page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Layout from "../Layout";
-import Header from "../Header";
+import Header from "../../foundations/ClaraHeader";
 import ListTags from "../ListTags";
 import Pagination from "../atoms/Pagination";
 import { INovelProp } from "../../lib/firebase/initFirebase";
