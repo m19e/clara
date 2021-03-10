d7bac81306471cd5b3321af2fb548073e1e2a0fd
m19e <nexus.me7290@gmail.com>
2021-03-10 20:55:00 +0900

    Must put children

--- components/molecules/Tooltip.tsx
+++ components/molecules/Tooltip.tsx
@@ -36,24 +36,9 @@ const Tooltip = ({
     return (
         <>
             <div className="w-10 h-10 mt-2 editor-bg rounded-full shadow-md flex-center">
-                {children ? (
-                    <div onMouseEnter={openLeftTooltip} onMouseLeave={closeLeftTooltip} ref={btnRef}>
-                        {children}
-                    </div>
-                ) : (
-                    <svg
-                        className={className}
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill={fill}
-                        viewBox={viewBox}
-                        stroke={stroke}
-                        onMouseEnter={openLeftTooltip}
-                        onMouseLeave={closeLeftTooltip}
-                        ref={btnRef}
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={d} />
-                    </svg>
-                )}
+                <div onMouseEnter={openLeftTooltip} onMouseLeave={closeLeftTooltip} ref={btnRef}>
+                    {children}
+                </div>
             </div>
             <div
                 className={(tooltipShow ? "" : "hidden ") + "editor-bg mr-4 block z-50 no-underline break-words rounded-xl border border-solid border-gray-300"}
