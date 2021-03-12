e4ef4cf8845892a1bb162ac8d086ed746c4cb3a4
m19e <nexus.me7290@gmail.com>
2021-03-12 19:53:48 +0900

    Shift tag icon to mol

--- components/organisms/NovelEditor.tsx
+++ components/organisms/NovelEditor.tsx
@@ -307,18 +307,6 @@ const NovelEditor = ({ novel, rootTags, rootR18, usedTags }: Props) => {
                                 </span>
                             </div>
                             <div className="flex items-center flex-wrap mr-1.5">
-                                {tags.length === 0 && (
-                                    <span className="w-4 h-4 mb-1 pl-1.5 flex-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={2}
-                                                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
-                                            />
-                                        </svg>
-                                    </span>
-                                )}
                                 <EditableTagList r18={r18} tags={tags} />
                                 <TagsEditModal tags={tags} setTags={setTags} />
                             </div>
