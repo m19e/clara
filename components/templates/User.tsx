495c0ef829855fb7b510ac09eff38db2c2dacbec
m19e <nexus.me7290@gmail.com>
2021-03-15 00:39:37 +0900

    Arrow func

--- components/templates/User.tsx
+++ components/templates/User.tsx
@@ -9,7 +9,7 @@ import ListTags from "../ListTags";
 const DISPLAY_NOVEL_SPAN = 5;
 const DEFAULT_CLARA_IMAGE = process.env.NEXT_PUBLIC_SITE_ROOT_URL + "/icon-128x128.png";
 
-export default function UserPage({ user, novels }: { user: UserProfile; novels: INovelDataSerializable[] }) {
+const User = ({ user, novels }: { user: UserProfile; novels: INovelDataSerializable[] }) => {
     const [rootList] = useState(novels);
     const [displayList, setDisplayList] = useState(novels.slice(0, DISPLAY_NOVEL_SPAN));
     const [hasMore, setHasMore] = useState(novels.length > DISPLAY_NOVEL_SPAN);
@@ -138,4 +138,6 @@ export default function UserPage({ user, novels }: { user: UserProfile; novels: 
             </div>
         </Layout>
     );
-}
+};
+
+export default User;
