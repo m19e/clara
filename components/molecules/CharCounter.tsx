71f4b38a82eb0ca974284f6c47b3e157884ce93d
m19e <nexus.me7290@gmail.com>
2021-03-15 20:01:29 +0900

    Fix declaration order

--- components/molecules/CharCounter.tsx
+++ components/molecules/CharCounter.tsx
@@ -1,6 +1,12 @@
 import { EditorState } from "draft-js";
 import { getTextCharCount } from "lib/novel/tools";
 
+const getEditorStateCharCount = (es: EditorState) => {
+    const text = es.getCurrentContent().getPlainText("");
+    const count = getTextCharCount(text);
+    return count;
+};
+
 type Props = {
     editorState: EditorState;
 };
@@ -8,12 +14,6 @@ type Props = {
 const CharCounter = ({ editorState }: Props) => {
     const count = getEditorStateCharCount(editorState);
     return <span className="mincho opacity-50">{count}字</span>;
-};
-
-const getEditorStateCharCount = (es: EditorState) => {
-    const text = es.getCurrentContent().getPlainText("");
-    const count = getTextCharCount(text);
-    return count;
 };
 
 export default CharCounter;
