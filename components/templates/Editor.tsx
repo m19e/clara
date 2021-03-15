c61176a61f91f9b261d74993f051f06bea6fa011
m19e <nexus.me7290@gmail.com>
2021-03-15 19:47:07 +0900

    Add draft editor template

---
+++ components/templates/Editor.tsx
@@ -0,0 +1,19 @@
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const DraftEditor = dynamic(() => import("components/DraftEditor"), { ssr: false });
+
+type Props = {
+    isMobile: boolean;
+};
+
+const Editor = ({ isMobile }: Props) => {
+    const router = useRouter();
+    if (isMobile) {
+        router.push("/");
+    }
+
+    return <DraftEditor />;
+};
+
+export default Editor;
