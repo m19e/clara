d6e28c08d1ee1dfe31410b2029886afdc6362b34
m19e <nexus.me7290@gmail.com>
2021-03-15 20:23:23 +0900

    Remove import

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,10 +1,6 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
 import { useUserAgent, UserAgent } from "next-useragent";
 import EditorPage from "components/templates/Editor";
-
-const DynamicEditor = dynamic(() => import("../../components/DraftEditor"), { ssr: false });
 
 const EditorIndex = ({ ua }: { ua: UserAgent }) => <EditorPage isMobile={ua.isMobile} />;
 
