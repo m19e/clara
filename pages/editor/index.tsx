4d806211db6ebf16e3fb78078bbc5b6a33f75cc5
m19e <nexus.me7290@gmail.com>
2021-03-15 19:47:20 +0900

    Replace by template

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -2,17 +2,11 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useUserAgent, UserAgent } from "next-useragent";
+import EditorPage from "components/templates/Editor";
 
 const DynamicEditor = dynamic(() => import("../../components/DraftEditor"), { ssr: false });
 
-const EditorIndex = ({ ua }: { ua: UserAgent }) => {
-    const router = useRouter();
-    if (ua.isMobile) {
-        router.push("/");
-    }
-
-    return <DynamicEditor />;
-};
+const EditorIndex = ({ ua }: { ua: UserAgent }) => <EditorPage isMobile={ua.isMobile} />;
 
 export const getServerSideProps: GetServerSideProps = async ({ req }: GetServerSidePropsContext) => {
     const ua = useUserAgent(req.headers["user-agent"]);
