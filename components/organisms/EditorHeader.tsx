5d910217397362450b7bc7315a0e0796c1d566d7
m19e <nexus.me7290@gmail.com>
2021-03-16 19:24:45 +0900

    Display title skeleton if loading

--- components/organisms/EditorHeader.tsx
+++ components/organisms/EditorHeader.tsx
@@ -6,7 +6,11 @@ import { useTitle, useDraftID, useIsTitleEdit } from "store/draft";
 import ClaraHeader from "foundations/ClaraHeader";
 import PublishModal from "components/organisms/PublishModal";
 
-const Header = () => {
+type Props = {
+    loading: boolean;
+};
+
+const Header = ({ loading }: Props) => {
     const [profile] = useProfile();
     const [draftID] = useDraftID();
     const [title, setTitle] = useTitle();
@@ -79,41 +83,47 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="py-5 flex-grow-0 flex-center group">
-                    {isTitleEdit ? (
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                className="text-gray-900 mx-2 text-center shadow-inner editor-bg rounded outline-none focus:outline-none"
-                                type="text"
-                                ref={editTitleRef}
-                                value={temptitle}
-                                onChange={handleTempTitleChange}
-                                onBlur={handleToggle}
-                                onKeyDown={(e) => {
-                                    if (e.key === "Tab") e.preventDefault();
-                                }}
-                                style={{ minWidth: "10rem", maxWidth: "50rem", width: `${temptitle.length + 1}rem` }}
-                            />
-                        </form>
+                    {loading ? (
+                        <div className="animate-pulse h-3 w-40 my-1.5 bg-gray-300 rounded"></div>
                     ) : (
                         <>
-                            <div className="w-6"></div>
-                            <p className="text-gray-900 whitespace-pre mx-1">{title}</p>
-                            <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-60" onClick={handleToggle}>
-                                <svg
-                                    className="w-4 h-4 opacity-50 hover:opacity-100"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="#2a2e3b"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                            {isTitleEdit ? (
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        className="text-gray-900 mx-2 text-center shadow-inner editor-bg rounded outline-none focus:outline-none"
+                                        type="text"
+                                        ref={editTitleRef}
+                                        value={temptitle}
+                                        onChange={handleTempTitleChange}
+                                        onBlur={handleToggle}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Tab") e.preventDefault();
+                                        }}
+                                        style={{ minWidth: "10rem", maxWidth: "50rem", width: `${temptitle.length + 1}rem` }}
                                     />
-                                </svg>
-                            </div>
+                                </form>
+                            ) : (
+                                <>
+                                    <div className="w-6"></div>
+                                    <p className="text-gray-900 whitespace-pre mx-1">{title}</p>
+                                    <div className="w-6 opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-60" onClick={handleToggle}>
+                                        <svg
+                                            className="w-4 h-4 opacity-50 hover:opacity-100"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="#2a2e3b"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                            />
+                                        </svg>
+                                    </div>
+                                </>
+                            )}
                         </>
                     )}
                 </div>
