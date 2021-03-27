08f24fb0af71a841c26972719e91a3ecaf0745be
m19e <nexus.me7290@gmail.com>
2021-03-27 21:55:38 +0900

    Display page number

--- components/molecules/LinkPagination.tsx
+++ components/molecules/LinkPagination.tsx
@@ -4,9 +4,16 @@ type Props = {
 };
 
 const LinkPagination = ({ currentPage, pageCount }: Props) => {
+    const prev = currentPage - 1;
+    const next = currentPage + 1;
+
     return (
-        <div>
-            {currentPage} in {pageCount}
+        <div className="inline-flex gap-2">
+            {currentPage != 1 && <span>1</span>}
+            {prev > 1 && <span>{prev}</span>}
+            {currentPage != 1 && currentPage !== pageCount && <span>{currentPage}</span>}
+            {next < pageCount && <span>{next}</span>}
+            {currentPage != pageCount && <span>{pageCount}</span>}
         </div>
     );
 };
