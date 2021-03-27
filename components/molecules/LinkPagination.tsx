8998a40c8c44600afc548d1e742492d0707b142b
m19e <nexus.me7290@gmail.com>
2021-03-28 06:11:20 +0900

    Set next link

--- components/molecules/LinkPagination.tsx
+++ components/molecules/LinkPagination.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Props = {
     currentPage: number;
     pageCount: number;
@@ -11,19 +13,35 @@ const LinkPagination = ({ currentPage, pageCount }: Props) => {
         <div className="inline-flex gap-2 mt-2">
             {currentPage != 1 && (
                 <>
-                    <span>{"<"}</span>
-                    <span>1</span>
+                    <Link href={`/page/${prev}`}>
+                        <a>{"<"}</a>
+                    </Link>
+                    <Link href="/page/1">
+                        <a>1</a>
+                    </Link>
                 </>
             )}
             {prev > 2 && <span>…</span>}
-            {prev > 1 && <span>{prev}</span>}
+            {prev > 1 && (
+                <Link href={`/page/${prev}`}>
+                    <a>{prev}</a>
+                </Link>
+            )}
             <span className="border-b border-gray-400">{currentPage}</span>
-            {next < pageCount && <span>{next}</span>}
+            {next < pageCount && (
+                <Link href={`/page/${next}`}>
+                    <a>{next}</a>
+                </Link>
+            )}
             {pageCount - next > 1 && <span>…</span>}
             {currentPage != pageCount && (
                 <>
-                    <span>{pageCount}</span>
-                    <span>{">"}</span>
+                    <Link href={`/page/${pageCount}`}>
+                        <a>{pageCount}</a>
+                    </Link>
+                    <Link href={`/page/${next}`}>
+                        <a>{">"}</a>
+                    </Link>
                 </>
             )}
         </div>
