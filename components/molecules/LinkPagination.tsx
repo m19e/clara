37b3453a0aadb923119773c83a27fb31a62125ef
m19e <nexus.me7290@gmail.com>
2021-03-27 22:42:13 +0900

    Add chevron, ellipsis control

--- components/molecules/LinkPagination.tsx
+++ components/molecules/LinkPagination.tsx
@@ -8,12 +8,24 @@ const LinkPagination = ({ currentPage, pageCount }: Props) => {
     const next = currentPage + 1;
 
     return (
-        <div className="inline-flex gap-2">
-            {currentPage != 1 && <span>1</span>}
+        <div className="inline-flex gap-2 mt-2">
+            {currentPage != 1 && (
+                <>
+                    <span>{"<"}</span>
+                    <span>1</span>
+                </>
+            )}
+            {prev > 2 && <span>…</span>}
             {prev > 1 && <span>{prev}</span>}
-            {currentPage != 1 && currentPage !== pageCount && <span>{currentPage}</span>}
+            <span className="border-b border-gray-400">{currentPage}</span>
             {next < pageCount && <span>{next}</span>}
-            {currentPage != pageCount && <span>{pageCount}</span>}
+            {pageCount - next > 1 && <span>…</span>}
+            {currentPage != pageCount && (
+                <>
+                    <span>{pageCount}</span>
+                    <span>{">"}</span>
+                </>
+            )}
         </div>
     );
 };
