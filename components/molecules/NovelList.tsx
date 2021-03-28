daf47ac8506e84b484462259ab1f662bd28d20c5
m19e <nexus.me7290@gmail.com>
2021-03-28 23:01:15 +0900

    Remove props

--- components/molecules/NovelList.tsx
+++ components/molecules/NovelList.tsx
@@ -3,11 +3,9 @@ import NovelListItem from "components/molecules/NovelListItem";
 
 type Props = {
     novels: INovelProp[];
-    pageCount: number;
-    initialPage: number;
 };
 
-const NovelList = ({ novels, pageCount, initialPage }: Props) => (
+const NovelList = ({ novels }: Props) => (
     <div className="w-3/4 xl:w-full grid grid-cols-1 xl:grid-cols-2 gap-12">
         {novels.map((novel, i) => (
             <NovelListItem key={i} novel={novel} className="flex flex-col justify-end border-b border-solid border-gray-300" />
