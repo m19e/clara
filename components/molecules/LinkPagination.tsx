104da01ce117587d2d1bd1eab3c1d6be53c4cdf5
m19e <nexus.me7290@gmail.com>
2021-03-28 21:46:55 +0900

    Add ellipsis fc

--- components/molecules/LinkPagination.tsx
+++ components/molecules/LinkPagination.tsx
@@ -8,6 +8,17 @@ const Chevron = ({ prev }: { prev: boolean }) => (
     </div>
 );
 
+const Ellipsis = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+        />
+    </svg>
+);
+
 type Props = {
     currentPage: number;
     pageCount: number;
