f2f0b795df4d5b7d92660b71c7064a6bfe9e535e
m19e <nexus.me7290@gmail.com>
2021-03-28 22:35:41 +0900

    Adjust gap

--- components/molecules/LinkPagination.tsx
+++ components/molecules/LinkPagination.tsx
@@ -35,7 +35,7 @@ const LinkPagination = ({ currentPage, pageCount }: Props) => {
     const next = currentPage + 1;
 
     return (
-        <div className="inline-flex items-center gap-2 mt-2">
+        <div className="inline-flex items-center gap-1 mt-2">
             {currentPage != 1 && (
                 <>
                     <Link href={`/page/${prev}`}>
