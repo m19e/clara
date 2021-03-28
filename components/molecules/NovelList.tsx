a3c98451c086895a2ada7214231cfdb56d17898d
m19e <nexus.me7290@gmail.com>
2021-03-28 23:00:34 +0900

    Remove needless markup

--- components/molecules/NovelList.tsx
+++ components/molecules/NovelList.tsx
@@ -1,6 +1,5 @@
 import { INovelProp } from "types";
 import NovelListItem from "components/molecules/NovelListItem";
-import LinkedPagination from "components/molecules/LinkPagination";
 
 type Props = {
     novels: INovelProp[];
@@ -9,15 +8,10 @@ type Props = {
 };
 
 const NovelList = ({ novels, pageCount, initialPage }: Props) => (
-    <div className="w-11/12 pt-12 xl:px-12 flex-center flex-col editor-bg rounded">
-        <div className="w-3/4 xl:w-full grid grid-cols-1 xl:grid-cols-2 gap-12">
-            {novels.map((novel, i) => (
-                <NovelListItem key={i} novel={novel} className="flex flex-col justify-end border-b border-solid border-gray-300" />
-            ))}
-        </div>
-        <div className="w-full flex-center my-8 editor-bg">
-            <LinkedPagination currentPage={initialPage + 1} pageCount={pageCount} />
-        </div>
+    <div className="w-3/4 xl:w-full grid grid-cols-1 xl:grid-cols-2 gap-12">
+        {novels.map((novel, i) => (
+            <NovelListItem key={i} novel={novel} className="flex flex-col justify-end border-b border-solid border-gray-300" />
+        ))}
     </div>
 );
 
