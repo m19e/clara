e9154cbd6bcb51f96d25f934c2c8c47f941732e0
m19e <nexus.me7290@gmail.com>
2021-03-30 23:52:26 +0900

    Typing ssr result

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import { INovelProp } from "types";
 import { getNovelsByIDs, getRootNovelIDs, PER_PAGE } from "lib/firebase/novel";
 import TopPage from "components/templates/Top";
@@ -10,7 +10,7 @@ type Props = {
 
 const TopIndex = ({ novels, pageCount }: Props) => <TopPage novels={novels} pageCount={pageCount} currentPage={1} />;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (): Promise<GetServerSidePropsResult<Props>> => {
     const novelIDs = await getRootNovelIDs();
     const novels = await getNovelsByIDs(novelIDs.slice(0, PER_PAGE));
     const pageCount = Math.ceil(novelIDs.length / PER_PAGE);
